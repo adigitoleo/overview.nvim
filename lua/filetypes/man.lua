08d9bca7ffc5c85cfffdc143ee926e1d888ae062
adigitoleo <adigitoleo@posteo.net>
2024-01-19 23:51:32 +1100

    fix: Fix incorrect match patterns for level 1 and 6 headings

--- lua/filetypes/man.lua
+++ lua/filetypes/man.lua
@@ -2,11 +2,11 @@ local Man = {}
 
 function Man.get_headings(man_text)
     local result = {}
-    local section_pattern = "^[%u]+%s.-$"
+    local section_pattern = "^[%u%s]+$"
     -- the "normal" text block is indented by 5 spaces for mdoc and 7 for man by default
     -- assume that anything with 4 or less leading spaces is a subsection header
     local subsection_pattern = "^%s%s?%s?%s?%a.-$"
-    local option_pattern = "^%s?[-][-]?%a.-$"
+    local option_pattern = "^%s*[-][-]?%a.-$"
     local line_number = 1
     for line in man_text:gmatch("([^\r\n]*)\r?\n?") do
         local section = line:match(section_pattern)
